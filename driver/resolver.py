0c051c937853892d9b39d0f31cabbd449d670506
Gyrfalcon <enguerrand.vie@efrei.net>
2023-01-18 16:17:46 +0100

    update

--- driver/resolver.py
+++ driver/resolver.py
@@ -1,4 +1,4 @@
-import time, os
+import time, os, requests, json
 from selenium import webdriver
 from collections import Counter
 from selenium.webdriver import ActionChains
@@ -13,6 +13,10 @@ headless = False # - HEADLESS - (no browser)
 discord = True
 windows = True
 webhook = ""
+version = "13.1.1"
+pass_all = True # - PASS ALL - (pass all champs)
+wait_time = 5 # - WAIT TIME - (time to wait between champ and results)
+separator = "|" # - SEPARATOR - (separator for data in data/champ_data.json)
 
 ############################
 
@@ -22,13 +26,53 @@ discord_log = ""
 def log_string(message):
     global discord_log
     discord_log += "\n" + message
+    print(message)
+    
+def get_champions_file():
+    with open("lol/champions.txt", "r") as f:
+        return f.read().splitlines()
+    
+def is_in_file(champ): # is champ already in file lol/champions.txt
+    file = get_champions_file()
+    for line in file:
+        if line == champ:
+            return True
+    return False
 
-# -- DEBUG --
-debug_time = False
-word_to_check = ""
+def is_in_data(champ): # is champ already in file data/champ_data.json
+    with open("data/champ_data.json", "r") as f:
+        for line in f:
+            if champ in line:
+                return True
+    return False
+
+def update_champions():
+    champs = []
+    res = requests.get("https://ddragon.leagueoflegends.com/cdn/"+version+"/data/fr_FR/champion.json")
+    if res.status_code != 200:
+        log_string("Error getting champions list : " + str(res.status_code)) # + champ + ".png"
+        return
+    res = res.json()
+    with open("lol/champions.txt", "a") as f:
+        for champ in res["data"]:
+            name = res["data"][champ]["name"]
+            if not is_in_file(name):
+                champs.append(name)
+                log_string("New champ : " + name)
+                f.writelines(name + "\n")
+    if len(champs) == 0:
+        log_string("No new champ")
+    return champs
+
+def split_caracteristic(caracteristic):
+    if "," in caracteristic:
+        return caracteristic.split(",")
+
+############################
 
 # -- VARIABLES --
 url = "https://loldle.net/classic"
+image_url = "https://ddragon.leagueoflegends.com/cdn/"+version+"/img/champion/"
 if windows:
     chromedriver_path = 'driver/chromedriver.exe'
     geckodriver_path = 'driver/geckodriver.exe'
@@ -38,6 +82,7 @@ if headless:
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
     if windows:
         driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
     else:
@@ -45,14 +90,50 @@ if headless:
 else:
     driver = webdriver.Firefox(executable_path=geckodriver_path)
 action = ActionChains(driver)
-driver.get(url)
-#driver.maximize_window()
+
+# -- START --
+new_champs = update_champions()
+driver.get(url) # -- GET URL --
 driver.implicitly_wait(5)
+driver.find_element(By.CLASS_NAME, "fc-cta-consent").click() # -- ACCEPT COOKIES --
 
-# Time
-start_time = time.time()
-global timerr
-timerr = time.time()
+if pass_all:
+    with open("lol/champions.txt", "r") as f:
+        champs = f.read().splitlines()
+        for champ in champs:
+            if champ == "Swain":
+                continue
+            driver.find_element(By.TAG_NAME, "body").send_keys(champ)
+            driver.find_element(By.TAG_NAME, "body").send_keys(Keys.ENTER)
+            time.sleep(wait_time)
+            squares = driver.find_elements(By.CLASS_NAME, "square")
+            nb_squares = len(squares)
+            gender = squares[nb_squares-7].text
+            positions = squares[nb_squares-6].text
+            species = squares[nb_squares-5].text 
+            resource = squares[nb_squares-4].text
+            range_type = squares[nb_squares-3].text
+            regions = squares[nb_squares-2].text
+            release_year = squares[nb_squares-1].text
+            validation = []
+            for i in range(1,8):
+                classe = squares[i].get_attribute("class")
+                if "square-good" in classe:
+                    validation.append("good")
+                elif "square-bad" in classe:
+                    validation.append("bad")
+                elif "square-inferior" in classe:
+                    validation.append("inferior")
+                elif "square-superior" in classe:
+                    validation.append("superior")
+                    validation.append("partial")
+            caracteristics = [str(gender), str(positions), str(species), str(resource), str(range_type), str(regions), str(release_year)]
+            with open("data/champ_data.json", "a") as f:
+                if not is_in_data(champ):
+                    line = {'name': champ, 'gender' : caracteristics[0], 'positions' : caracteristics[1], 'species' : caracteristics[2], 'resource' : caracteristics[3], 'range_type' : caracteristics[4], 'regions' : caracteristics[5], 'release_year' : caracteristics[6]}
+                    f.write(json.dumps(line) + "\n")
+                    log_string("New champ data : " + champ)
 
-while True:
-    pass
+
+time.sleep(3)
+driver.close()
